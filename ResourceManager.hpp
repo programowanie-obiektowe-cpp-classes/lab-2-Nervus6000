20255a1533c1e6cd10c29c84467cf219e9c086e5
Nervus6000 <cyg.kamil@gmail.com>
2025-10-25 18:51:00 +0200

    zadanie 1 i 2

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,42 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+private:
+    Resource* resource_ptr; 
+
+public:
+    ResourceManager()
+    {
+        resource_ptr = new Resource();
+    }
+
+    ~ResourceManager()
+    {
+        delete resource_ptr;
+    }
+
+    ResourceManager(const ResourceManager& other)
+    {
+        resource_ptr = new Resource( *(other.resource_ptr) );
+    }
+
+    ResourceManager& operator=(const ResourceManager& other)
+    {
+        if (this == &other) {
+            return *this;
+        }
+        delete resource_ptr;
+        resource_ptr = new Resource( *(other.resource_ptr) );
+        return *this;
+    }
+
+    ResourceManager(ResourceManager&& other) = delete;
+    ResourceManager& operator=(ResourceManager&& other) = delete;
+
+    double get()
+    {
+        return (*resource_ptr).get();
+    }
 };
+
+
